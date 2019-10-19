e76f959114dff2301a839ed449bbf84f8d120d30
Arthur Moreno <morenoarthur52@gmail.com>
2019-10-19 04:45:09 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/morenoweb/mongodb/service/exception/ObjectNotFoundException.java
@@ -0,0 +1,17 @@
+package com.morenoweb.mongodb.service.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	
+	
+	
+	
+	
+
+}
