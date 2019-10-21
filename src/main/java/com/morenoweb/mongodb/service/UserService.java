e75af86933d98b8b854001465efc987c2c23bf46
Arthur Moreno <morenoarthur52@gmail.com>
2019-10-21 09:17:41 -0300

    Post , Delete , Put

--- src/main/java/com/morenoweb/mongodb/service/UserService.java
+++ src/main/java/com/morenoweb/mongodb/service/UserService.java
@@ -7,22 +7,53 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.morenoweb.mongodb.domain.User;
+import com.morenoweb.mongodb.dto.UserDTO;
 import com.morenoweb.mongodb.repository.UserRepository;
 import com.morenoweb.mongodb.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public void deleteById(String id) {
+
+		if (!(findById(id) == null)) {
+
+			repo.deleteById(id);
+
 		}
+
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 
 }
