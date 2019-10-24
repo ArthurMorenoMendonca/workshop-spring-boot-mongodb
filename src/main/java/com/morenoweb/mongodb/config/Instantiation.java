aaa0f809bfdd6278d5ea19f01195f6112a2f5b3a
Arthur Moreno <morenoarthur52@gmail.com>
2019-10-24 09:12:13 -0300

    Criando entity Post com User aninhado e Projeção dos dados do autor com DTO

--- src/main/java/com/morenoweb/mongodb/config/Instantiation.java
+++ src/main/java/com/morenoweb/mongodb/config/Instantiation.java
@@ -1,12 +1,17 @@
 package com.morenoweb.mongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.morenoweb.mongodb.domain.Post;
 import com.morenoweb.mongodb.domain.User;
+import com.morenoweb.mongodb.dto.AuthorDTO;
+import com.morenoweb.mongodb.repository.PostRepository;
 import com.morenoweb.mongodb.repository.UserRepository;
 
 @Configuration
@@ -14,11 +19,18 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sd = new SimpleDateFormat("dd/MM/yyyy");
+		sd.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
@@ -26,6 +38,11 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
+		
+		Post post1 = new Post(null, sd.parse("21/03/2018"), "Partiu viagem", "Vou viajar para são paulo!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sd.parse("23/03/2018"), "Bom Dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 	
 	
